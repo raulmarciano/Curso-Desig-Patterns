d9f3a720e2266d5807bdbc4fc6af2b1bff44905f
Raul <raul_marciano@outlook.com>
2020-11-26 15:07:55 -0300

    Os tipos de impostos e suas implementações

---
+++ CursoDesignPatterns/CursoDesignPatterns/Strategy/ICMS.cs
@@ -0,0 +1,10 @@
+﻿namespace CursoDesignPatterns.Strategy
+{
+    public class ICMS : IImposto
+    {
+        public double Calcula(Orcamento orcamento)
+        {
+            return orcamento.Valor * 0.1;
+        }
+    }
+}
---
+++ CursoDesignPatterns/CursoDesignPatterns/Strategy/ISS.cs
@@ -0,0 +1,10 @@
+﻿namespace CursoDesignPatterns.Strategy
+{
+    public class ISS : IImposto
+    {
+        public double Calcula(Orcamento orcamento)
+        {
+            return orcamento.Valor * 0.06;
+        }
+    }
+}
