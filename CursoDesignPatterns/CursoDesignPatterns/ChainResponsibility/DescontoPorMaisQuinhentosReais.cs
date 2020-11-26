cf666430378d0e9ec39cc66a8dae6dcdec3e2d34
Raul <raul_marciano@outlook.com>
2020-11-26 16:00:01 -0300

    Tipos de descontos

---
+++ CursoDesignPatterns/CursoDesignPatterns/ChainResponsibility/DescontoPorMaisQuinhentosReais.cs
@@ -0,0 +1,15 @@
+﻿namespace CursoDesignPatterns.ChainResponsibility
+{
+    public class DescontoPorMaisQuinhentosReais : IDesconto
+    {
+        public IDesconto Proximo { get; set; }
+
+        public double Desconta(Orcamento orcamento)
+        {
+            if (orcamento.Valor > 550)
+                return orcamento.Valor * 0.07;
+
+            return Proximo.Desconta(orcamento);
+        }
+    }
+}
