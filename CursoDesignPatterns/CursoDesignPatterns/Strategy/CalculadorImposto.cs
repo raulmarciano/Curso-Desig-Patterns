1494fbff45db5b5fb6c2d321f6c3ee7b7a6492ff
Raul <raul_marciano@outlook.com>
2020-11-26 15:09:54 -0300

    Classe que chama o cálculo de imposto

---
+++ CursoDesignPatterns/CursoDesignPatterns/Strategy/CalculadorImposto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CursoDesignPatterns.Strategy
+{
+    public class CalculadorImposto
+    {
+        public void RealizaCalculo(Orcamento orcamento, IImposto imposto)
+        {
+            double valorImposto = imposto.Calcula(orcamento);
+            Console.WriteLine(valorImposto);
+        }
+    }
+}
