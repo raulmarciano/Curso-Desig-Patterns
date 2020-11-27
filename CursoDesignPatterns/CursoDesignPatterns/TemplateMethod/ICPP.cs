3498d64e16a5609eff8f9e13f8dd9a8ed0fdc0a2
Raul Marciano <57970226+raulmarciano@users.noreply.github.com>
2020-11-26 21:15:42 -0300

    Classe dos impostos herdando o template

---
+++ CursoDesignPatterns/CursoDesignPatterns/TemplateMethod/ICPP.cs
@@ -0,0 +1,22 @@
+﻿using CursoDesignPatterns.Strategy;
+
+namespace CursoDesignPatterns.TemplateMethod
+{
+    public class ICPP : TemplateImpostoCondicional
+    {
+        public override bool UsarMaximaTaxacao(Orcamento orcamento)
+        {
+            return orcamento.Valor >= 500;
+        }
+
+        public override double MaximaTaxacao(Orcamento orcamento)
+        {
+            return orcamento.Valor * 0.07;
+        }
+
+        public override double MinimaTaxacao(Orcamento orcamento)
+        {
+            return orcamento.Valor * 0.05;
+        }
+    }
+}
