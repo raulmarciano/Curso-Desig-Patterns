b60faf8629a5d4530f1b4b1037cb4c1fa88ce666
Raul Marciano <57970226+raulmarciano@users.noreply.github.com>
2020-12-30 20:54:19 -0300

    Correção do valor do ICMS

--- CursoDesignPatterns/CursoDesignPatterns/Decorator/ICMS.cs
+++ CursoDesignPatterns/CursoDesignPatterns/Decorator/ICMS.cs
@@ -12,7 +12,7 @@ namespace CursoDesignPatterns.Decorator
 
         public override double Calcula(Orcamento orcamento)
         {
-            return orcamento.Valor * 0.06 + CalculoDoOutroImposto(orcamento);
+            return orcamento.Valor * 0.1 + CalculoDoOutroImposto(orcamento);
         }
     }
 }
